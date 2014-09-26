c26aaf5d4d55c7ed124276f7c0303fe11484a42e
E Ananthaneshan <poornan@gmail.com>
2014-09-26 11:54:37 +0530

    test suit added

--- org.wso2.carbon.connector/src/test/java/org/wso2/carbon/connector/integration/test/meetup/MeetupConnectorIntegrationTest.java
+++ org.wso2.carbon.connector/src/test/java/org/wso2/carbon/connector/integration/test/meetup/MeetupConnectorIntegrationTest.java
@@ -1,20 +1,20 @@
 /*
-*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * 
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.carbon.connector.integration.test.meetup;
 
 import org.apache.axis2.context.ConfigurationContext;
@@ -34,111 +34,153 @@ import javax.activation.DataHandler;
 import java.net.URL;
 import java.util.Properties;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MeetupConnectorIntegrationTest.
+ */
 public class MeetupConnectorIntegrationTest extends ESBIntegrationTest {
 
+	/** The Constant CONNECTOR_NAME. */
+	protected static final String CONNECTOR_NAME = "meetup";
 
-    protected static final String CONNECTOR_NAME = "meetup";
+	/** The mediation lib upload stub. */
+	protected MediationLibraryUploaderStub mediationLibUploadStub;
 
-    protected MediationLibraryUploaderStub mediationLibUploadStub;
+	/** The admin service stub. */
+	protected MediationLibraryAdminServiceStub adminServiceStub;
 
-    protected MediationLibraryAdminServiceStub adminServiceStub;
+	/** The proxy admin. */
+	protected ProxyServiceAdminClient proxyAdmin;
 
-    protected ProxyServiceAdminClient proxyAdmin;
+	/** The repo location. */
+	protected String repoLocation;
 
-    protected String repoLocation;
+	/** The meetup connector file name. */
+	protected String meetupConnectorFileName = CONNECTOR_NAME + ".zip";
 
-    protected String meetupConnectorFileName = CONNECTOR_NAME + ".zip";
+	/** The meetup connector properties. */
+	protected Properties meetupConnectorProperties;
 
-    protected Properties meetupConnectorProperties;
+	/** The path to proxies directory. */
+	protected String pathToProxiesDirectory;
 
-    protected String pathToProxiesDirectory;
+	/** The path to requests directory. */
+	protected String pathToRequestsDirectory;
 
-    protected String pathToRequestsDirectory;
+	/**
+	 * Sets the environment.
+	 * 
+	 * @throws Exception
+	 *             the exception
+	 */
+	@BeforeClass(alwaysRun = true)
+	public void setEnvironment() throws Exception {
 
+		super.init();
 
-    @BeforeClass(alwaysRun = true)
-    public void setEnvironment() throws Exception {
+		ConfigurationContextProvider configurationContextProvider =
+		                                                            ConfigurationContextProvider.getInstance();
+		ConfigurationContext cc = configurationContextProvider.getConfigurationContext();
+		mediationLibUploadStub =
+		                         new MediationLibraryUploaderStub(cc, esbServer.getBackEndUrl() +
+		                                                              "MediationLibraryUploader");
+		AuthenticateStub.authenticateStub("admin", "admin", mediationLibUploadStub);
 
-        super.init();
+		adminServiceStub =
+		                   new MediationLibraryAdminServiceStub(cc, esbServer.getBackEndUrl() +
+		                                                            "MediationLibraryAdminService");
 
-        ConfigurationContextProvider configurationContextProvider = ConfigurationContextProvider.getInstance();
-        ConfigurationContext cc = configurationContextProvider.getConfigurationContext();
-        mediationLibUploadStub =
-                new MediationLibraryUploaderStub(cc, esbServer.getBackEndUrl() + "MediationLibraryUploader");
-        AuthenticateStub.authenticateStub("admin", "admin", mediationLibUploadStub);
+		AuthenticateStub.authenticateStub("admin", "admin", adminServiceStub);
 
-        adminServiceStub =
-                new MediationLibraryAdminServiceStub(cc, esbServer.getBackEndUrl() + "MediationLibraryAdminService");
+		if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+			repoLocation = System.getProperty("connector_repo").replace("/", "\\");
+		} else {
+			repoLocation = System.getProperty("connector_repo").replace("/", "/");
+		}
+		proxyAdmin =
+		             new ProxyServiceAdminClient(esbServer.getBackEndUrl(),
+		                                         esbServer.getSessionCookie());
 
-        AuthenticateStub.authenticateStub("admin", "admin", adminServiceStub);
+		ConnectorIntegrationUtil.uploadConnector(repoLocation, mediationLibUploadStub,
+		                                         meetupConnectorFileName);
+		log.info("Sleeping for " + 20000 / 1000 + " seconds while waiting for synapse import");
+		Thread.sleep(20000);
 
-        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-            repoLocation = System.getProperty("connector_repo").replace("/", "\\");
-        } else {
-            repoLocation = System.getProperty("connector_repo").replace("/", "/");
-        }
-        proxyAdmin = new ProxyServiceAdminClient(esbServer.getBackEndUrl(), esbServer.getSessionCookie());
+		adminServiceStub.updateStatus("{org.wso2.carbon.connector}" + CONNECTOR_NAME,
+		                              CONNECTOR_NAME, "org.wso2.carbon.connector", "enabled");
 
-        ConnectorIntegrationUtil.uploadConnector(repoLocation, mediationLibUploadStub, meetupConnectorFileName);
-        log.info("Sleeping for " + 15000 / 1000 + " seconds while waiting for synapse import");
-        Thread.sleep(15000);
+		meetupConnectorProperties =
+		                            ConnectorIntegrationUtil.getConnectorConfigProperties(CONNECTOR_NAME);
 
-        adminServiceStub.updateStatus("{org.wso2.carbon.connector}" + CONNECTOR_NAME, CONNECTOR_NAME,
-                "org.wso2.carbon.connector", "enabled");
+		pathToProxiesDirectory =
+		                         repoLocation +
+		                                 meetupConnectorProperties.getProperty("proxyDirectoryRelativePath");
+		pathToRequestsDirectory =
+		                          repoLocation +
+		                                  meetupConnectorProperties.getProperty("requestDirectoryRelativePath");
 
-        meetupConnectorProperties = ConnectorIntegrationUtil.getConnectorConfigProperties(CONNECTOR_NAME);
+	}
 
-        pathToProxiesDirectory = repoLocation + meetupConnectorProperties.getProperty("proxyDirectoryRelativePath");
-        pathToRequestsDirectory = repoLocation + meetupConnectorProperties.getProperty("requestDirectoryRelativePath");
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.wso2.carbon.esb.ESBIntegrationTest#cleanup()
+	 */
+	@Override
+	protected void cleanup() throws Exception {
+		axis2Client.destroy();
+	}
 
-    }
+	/**
+	 * Test get open events with required parameters.
+	 * 
+	 * @throws Exception
+	 *             the exception
+	 */
+	@Test(enabled = false, groups = { "wso2.esb" }, description = "meetup {getOpenEvents} integration test")
+	public void testGetOpenEventsWithRequiredParameters() throws Exception {
 
-    @Override
-    protected void cleanup() throws Exception {
-        axis2Client.destroy();
-    }
+		String jsonRequestFilePath = pathToRequestsDirectory + "getOpenEvents_mandatory.txt";
+		String methodName = "meetup_get_open_events";
 
+		final String jsonString = ConnectorIntegrationUtil.getFileContent(jsonRequestFilePath);
+		final String proxyFilePath = "file:///" + pathToProxiesDirectory + methodName + ".xml";
+		String modifiedJsonString =
+		                            String.format(jsonString,
+		                                          meetupConnectorProperties.getProperty("key"),
+		                                          // meetupConnectorProperties.getProperty("category"),
+		                                          // meetupConnectorProperties.getProperty("city"),
+		                                          // meetupConnectorProperties.getProperty("country"),
+		                                          // meetupConnectorProperties.getProperty("lat"),
+		                                          // meetupConnectorProperties.getProperty("lon"),
+		                                          // meetupConnectorProperties.getProperty("state"),
+		                                          // meetupConnectorProperties.getProperty("text"),
+		                                          // meetupConnectorProperties.getProperty("topic"),
+		                                          meetupConnectorProperties.getProperty("zip"));
+		proxyAdmin.addProxyService(new DataHandler(new URL(proxyFilePath)));
 
-    @Test(enabled = false, groups = {"wso2.esb"}, description = "meetup {getOpenEvents} integration test")
-    public void testGetOpenEventsWithRequiredParameters() throws Exception {
+		try {
 
-        String jsonRequestFilePath = pathToRequestsDirectory + "getOpenEvents_mandatory.txt";
-        String methodName = "meetup_get_open_events";
+			// int responseHeader =
+			// ConnectorIntegrationUtil.sendRequestToRetriveHeaders(getProxyServiceURL(methodName),
+			// modifiedJsonString);
+			// Assert.assertTrue(responseHeader == 200);
+			JSONObject jsonObject =
+			                        ConnectorIntegrationUtil.sendRequest(getProxyServiceURL(methodName),
+			                                                             modifiedJsonString);
+			// Assert.assertTrue(jsonObject.has("results"));
+			// System.out.println("--------------@@@@@@@@---------");
+			// System.out.println(jsonObject);
 
-        final String jsonString = ConnectorIntegrationUtil.getFileContent(jsonRequestFilePath);
-        final String proxyFilePath = "file:///" + pathToProxiesDirectory + methodName + ".xml";
-        String modifiedJsonString = String.format(jsonString,
-               meetupConnectorProperties.getProperty("key"),
-//                meetupConnectorProperties.getProperty("category"),
-//                meetupConnectorProperties.getProperty("city"),
-//                meetupConnectorProperties.getProperty("country"),
-//                meetupConnectorProperties.getProperty("lat"),
-//                meetupConnectorProperties.getProperty("lon"),
-//                meetupConnectorProperties.getProperty("state"),
-//                meetupConnectorProperties.getProperty("text"),
-//                meetupConnectorProperties.getProperty("topic"),
-                meetupConnectorProperties.getProperty("zip")
-        );
-        proxyAdmin.addProxyService(new DataHandler(new URL(proxyFilePath)));
+			int responseHeader =
+			                     ConnectorIntegrationUtil.sendRequestToRetriveHeaders(getProxyServiceURL(methodName),
+			                                                                          modifiedJsonString);
 
+			Assert.assertTrue(responseHeader == 200);
 
-        try {
-
-            //int responseHeader = ConnectorIntegrationUtil.sendRequestToRetriveHeaders(getProxyServiceURL(methodName), modifiedJsonString);
-            //Assert.assertTrue(responseHeader == 200);
-            JSONObject jsonObject = ConnectorIntegrationUtil.sendRequest(getProxyServiceURL(methodName), modifiedJsonString);
-            //Assert.assertTrue(jsonObject.has("results"));
-            //System.out.println("--------------@@@@@@@@---------");
-            //System.out.println(jsonObject);
-
-            int responseHeader = ConnectorIntegrationUtil.sendRequestToRetriveHeaders(getProxyServiceURL(methodName), modifiedJsonString);
-
-            Assert.assertTrue(responseHeader == 200);
-
-
-        } finally {
-            proxyAdmin.deleteProxy(methodName);
-        }
-    }
+		} finally {
+			proxyAdmin.deleteProxy(methodName);
+		}
+	}
 
 }
